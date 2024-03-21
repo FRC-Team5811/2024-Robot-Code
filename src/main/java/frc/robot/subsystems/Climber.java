239aabacf696fd1e5c33e5fbe4d2c6a61834d327
oliver-sp <oliver.t.spaulding@gmail.com>
2024-03-21 17:45:49 -0400

    things I forgot to commit

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -3,12 +3,14 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
 
     public final CANSparkMax climberMotor = new CANSparkMax(16, MotorType.kBrushless);
     public final double climbSpeed = 1;
+    private final DigitalInput limitSwitch = new DigitalInput(1);
 
     public Climber () {
         climberMotor.setInverted(false);
@@ -24,6 +26,10 @@ public class Climber extends SubsystemBase {
 
     public void stop() {
         climberMotor.stopMotor();
+    }
+
+    public boolean getLimitBool() {
+        return limitSwitch.get();
     }
 
 
